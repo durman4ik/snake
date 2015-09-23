f0661a89043177de0f8d702d0dd258461b0f149f
Durman4ik <yur4ik93@mail.ru>
2015-09-23 16:00:20 +0300

    List of points

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,10 +10,21 @@ namespace Snake
         static void Main(string[] args)
         {
             Point p1 = new Point(1, 3, '*');
-            p1.Draw();
-
             Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            Point p3 = new Point(2, 6, '%');
+            Point p4 = new Point(3, 2, '@');
+            
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
 
             Console.ReadLine();
         }
