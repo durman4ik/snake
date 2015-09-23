5ff66740acc17a468b663a51cd32630e2d1c5c62
Durman4ik <yur4ik93@mail.ru>
2015-09-23 16:38:16 +0300

    Horizontal and Vertical lines

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,21 +10,16 @@ namespace Snake
         static void Main(string[] args)
         {
             Point p1 = new Point(1, 3, '*');
+            p1.Draw();
+
             Point p2 = new Point(4, 5, '#');
-            Point p3 = new Point(2, 6, '%');
-            Point p4 = new Point(3, 2, '@');
-            
+            p2.Draw();
 
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-            pList.Add(p3);
-            pList.Add(p4);
+            HorizontalLine hLine = new HorizontalLine(5, 10, 8, '+');
+            hLine.Drow();
 
-            foreach (Point p in pList)
-            {
-                p.Draw();
-            }
+            VerticalLine vLine = new VerticalLine(4, 7, 12, '&');
+            vLine.Drow();
 
             Console.ReadLine();
         }
